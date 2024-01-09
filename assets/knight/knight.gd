953f5a3492279c91e54d998f9f44b9f75ea06d51
Kamil <kami.krasowski.uk@gmail.com>
2024-01-09 20:53:14 +0000

    Added knight movement

---
+++ assets/knight/knight.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+func play_idle_animation():
+	%AnimatedSprite2D.play("idle")
+	
+	
+func play_run_animation():
+	%AnimatedSprite2D.play("run")
--- player.gd
+++ player.gd
@@ -1,6 +1,11 @@
-extends Node2D
+extends CharacterBody2D
 
-@onready var anim = $AnimatedSprite2D
-
-func _ready():
-	anim.play("run")
+func _physics_process(delta):
+	var direction = Input.get_vector("move_left", "move_right", "move_up","move_down")
+	velocity = direction * 600
+	move_and_slide()
+	
+	if velocity.length() > 0.0:
+		%knight.play_run_animation()
+	else:
+		%knight.play_idle_animation()
