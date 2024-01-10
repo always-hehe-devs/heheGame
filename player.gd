056165060acf1e06e79140af83b3f65fa8efece7
Kamil <kami.krasowski.uk@gmail.com>
2024-01-10 21:39:26 +0000

    Added tile assets

--- player.gd
+++ player.gd
@@ -1,11 +1,24 @@
 extends CharacterBody2D
 
+
+@export var speed: float = 300.0
+@export var jump_velocity = -200.0
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
 func _physics_process(delta):
-	var direction = Input.get_vector("move_left", "move_right", "move_up","move_down")
-	velocity = direction * 600
-	move_and_slide()
-	
-	if velocity.length() > 0.0:
-		%knight.play_run_animation()
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_velocity
+
+
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * speed
 	else:
-		%knight.play_idle_animation()
+		velocity.x = move_toward(velocity.x, 0, speed)
+
+	move_and_slide()
